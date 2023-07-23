483f4bc4fae5db94f888f24b3ec27de765717941
Stephen Chen <stephenchenKL@gmail.com>
2023-07-22 21:12:49 -0700

    use condition variable

--- main.cpp
+++ main.cpp
@@ -13,6 +13,15 @@ std::mutex cv_m;
 
 using namespace std::chrono_literals;
 
+enum CMD{
+    invalid = 0,
+    quit = 1,
+    read_sensor = 2,
+    reset_sensor =3
+};
+
+CMD cmd = invalid;
+
 void mqtt_sub_atomic_boolean(MqttDataClient& mqttDataClient)
 {
     mqttDataClient.loop_start();
@@ -24,6 +33,37 @@ void mqtt_sub_atomic_boolean(MqttDataClient& mqttDataClient)
     mqttDataClient.disconnect();
     mqttDataClient.loop_stop(false);
 }
+
+void sensor_svr(MqttDataClient& mqttDataClient)
+{
+    mqttDataClient.loop_start();
+    std::unique_lock<std::mutex> ul(cv_m);  //lock is applied on mutex m by thread t1
+    while(true){
+
+        cv.wait(ul,[] {return cmd != invalid;});  //waits until the condition becomes true
+
+        if (cmd == quit){
+            break;
+        }
+        else if (cmd == read_sensor){
+            std::cout << "read sensor \n";
+            //clear cmd
+            cmd = invalid;
+        }
+        else if (cmd == reset_sensor){
+            std::cout << "reset sensor \n";
+            //clear cmd
+            cmd = invalid;
+        }
+
+
+    }
+     
+
+    mqttDataClient.disconnect();
+    mqttDataClient.loop_stop(false);
+}
+
 
 
 
@@ -83,10 +123,20 @@ int main(int argc, char *argv[])
         std::cout << msg->qos << std::endl;
         std::cout << msg->retain << std::endl;
 
-        if (std::strcmp( static_cast<const char*> (msg->payload), "stop") == 0){
-            std::cout << "Stopping mqtt_sub_atomic_boolean ...\n" ;
-            running = false;
-
+        if (std::strcmp( static_cast<const char*> (msg->payload), "quit") == 0){
+            std::cout << "command received: quit\n" ;
+            cmd = quit;
+            cv.notify_all();
+        }
+        else if (std::strcmp( static_cast<const char*> (msg->payload), "read_sensor") == 0){
+            std::cout << "command received: read_sensor\n" ;
+            cmd = read_sensor;
+            cv.notify_all();
+        }
+        else if (std::strcmp( static_cast<const char*> (msg->payload), "reset_sensor") == 0){
+            std::cout << "command received: read_sensor\n" ;
+            cmd = reset_sensor;
+            cv.notify_all();
         }
         
     };
@@ -96,7 +146,7 @@ int main(int argc, char *argv[])
     mqttDataClient.register_on_message("topic_command", topic_command_cb);
 
 
-    std::thread t1(mqtt_sub_atomic_boolean, std::ref(mqttDataClient));
+    std::thread t1(sensor_svr, std::ref(mqttDataClient));
 
     t1.join();
 
